f13d3b04071321a431d5cc9c46a4c410a824cfeb
Lee Winter <lee.winter@boxspark.co.uk>
2025-01-06 18:03:00 +0000

    Change theme colors

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,7 @@
-import { css } from "@emotion/react";
 import { AppBar, Toolbar, useTheme } from "@mui/material";
 import { NavLink, Outlet } from "react-router-dom";
+
+import { css } from "@emotion/react";
 
 export default function Layout() {
   const theme = useTheme();
@@ -8,17 +9,17 @@ export default function Layout() {
   const styles = css`
     a {
       padding: ${theme.spacing(0.8)};
-      color: ${theme.palette.text.primary};
+      color: ${theme.palette.primary.contrastText}; /* Ensure contrast with AppBar */
       text-decoration: none;
       font-weight: 500;
       transition: color 0.3s ease;
 
       &.active {
-        color: ${theme.palette.secondary.dark};
+        color: ${theme.palette.secondary.light}; /* Highlight active link */
       }
 
       &:hover {
-        color: ${theme.palette.primary.light};
+        color: ${theme.palette.secondary.dark}; /* Hover effect */
       }
     }
   `;
