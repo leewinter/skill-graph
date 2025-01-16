268ca7ada1133d77c94ccb8f98cd37baf1b26ee3
Lee Winter <lee.winter@boxspark.co.uk>
2025-01-16 06:45:35 +0000

    Remove all traces of tabulator

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,8 @@ import { HashRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import en from "@src/locales/en.json";
-import es from "@src/locales/es.json"
-import { initI18n } from "@src/utils/i18n.ts"
+import es from "@src/locales/es.json";
+import { initI18n } from "@src/utils/i18n.ts";
 import theme from "@src/theme";
 
 initI18n({
@@ -19,23 +19,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      {/* Adding Global Styles */}
-      <Global
-        styles={css`
-          div.tabulator-popup-container {
-            position: absolute;
-            left: 100px;
-            background: ${theme.palette.background.paper};
-            cursor: pointer;
-            padding: 3px;
-            border: 2px solid ${theme.palette.divider};
-            border-radius: 5px;
-          }
-          .tabulator-edit-list-item.active {
-            background: ${theme.palette.secondary.light};
-          }
-        `}
-      />
       <HashRouter>
         <App />
       </HashRouter>
